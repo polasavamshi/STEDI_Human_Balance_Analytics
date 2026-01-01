dede6292c7e4a8e837d2a6034bcb8694954dfe2a
vamshi krishna <123619340+polasavamshi@users.noreply.github.com>
2026-01-01 19:28:07 +0530

    Add files via upload

---
+++ DDL/accelerometer_landing.sql
@@ -0,0 +1,16 @@
+CREATE EXTERNAL TABLE `accelorometer_landing_table`(
+  `user` string COMMENT 'from deserializer', 
+  `timestamp` bigint COMMENT 'from deserializer', 
+  `x` double COMMENT 'from deserializer', 
+  `y` double COMMENT 'from deserializer', 
+  `z` double COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://sparkglueproject/Accelorometer/landing/'
+TBLPROPERTIES (
+  'classification'='json')    
